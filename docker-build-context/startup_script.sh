48bfb5d9b71b70a913759e1260d2cc8aa50f30a7
gajdi88 <70482516+gajdi88@users.noreply.github.com>
2024-07-02 20:48:50 +0100

    init

---
+++ docker-build-context/startup_script.sh
@@ -0,0 +1,21 @@
+# This file needs to be an executable shell script
+#!/bin/bash
+
+# write the outputs of the script to a log file
+exec > >(tee /var/log/startup_script.log) 2>&1
+
+# read nordvpn token from txt
+token=$(<token.txt)
+
+# This script is used to start the NordVPN service and connect to a specific server
+nordvpn login --token $token
+
+nordvpn set killswitch 1
+nordvpn set analytics 0
+nordvpn whitelist add subnet 192.168.0.0/24
+sleep 5
+nordvpn c
+sleep 5
+nordvpn d
+sleep 5
+nordvpn c Netherlands
