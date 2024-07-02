7c0ffa2bd162e223dd766b77e4dc18e7036affbb
gajdi88 <70482516+gajdi88@users.noreply.github.com>
2024-07-02 22:22:22 +0100

    expose ports

--- docker-build-context/startup_script.sh
+++ docker-build-context/startup_script.sh
@@ -5,10 +5,21 @@
 
 # Add a delay to ensure the service starts properly
 sleep 5
+# diagnostics -----------------
 
-# write the outputs of the script to a log file
-# exec > >(tee /var/log/startup_script.log) 2>&1
+# Check the status of the NordVPN service
+#/etc/init.d/nordvpn status
 
+# Verify if the socket file is created
+if [ -e /run/nordvpn/nordvpnd.sock ]; then
+    echo "NordVPN service started successfully."
+else
+    echo "NordVPN service failed to start. /run/nordvpn/nordvpnd.sock not found."
+    exit 1
+fi
+
+
+# -------------------------
 # read nordvpn token from txt
 token=$(</root/token.txt)
 
@@ -19,6 +30,17 @@ nordvpn set killswitch 1
 nordvpn set analytics 0
 nordvpn whitelist add subnet 192.168.0.0/24
 sleep 5
+
+#echo "Attempting to connect to NordVPN..."
+#nordvpn connect --log --debug
+#if [ $? -ne 0 ]; then
+#    echo "NordVPN connection failed. Debug logs:"
+#    nordvpn logs
+#    exit 1
+#fi
+
+
+# previous connect
 nordvpn c
 sleep 5
 nordvpn d
